d82cebaddb20de9119d9be652ba81332cfebb996
Maxime Bonnesoeur <44313994+peterbonnesoeur@users.noreply.github.com>
2026-07-06 10:43:14 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        if len(prices) == 0:
+            return 0
+
+        # read the text before...
+        # we only do 1 transcation. we need to remember the last min of our window and the bigger max...
+        
+        curr_min = prices[0]
+        max_return = 0
+        for i in range(1, len(prices)):
+            if prices[i-1] < prices[i]:
+                max_return  =  max(prices[i] - curr_min, max_return)
+                # curr_min = prices[i]
+            else:
+                curr_min = min(prices[i], curr_min)
+
+        return max_return
+        
