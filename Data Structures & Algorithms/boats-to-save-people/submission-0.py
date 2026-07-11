9d4e776db897cee557add9a3e0d0c6bf489139c3
Maxime Bonnesoeur <44313994+peterbonnesoeur@users.noreply.github.com>
2026-07-11 15:54:05 +0200

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.py
@@ -0,0 +1,76 @@
+from collections import Counter
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        """
+            This sounds like a greedy exercise
+
+            No, only 2 people, we can get smarter
+
+            count = Counter(people)
+            num_boats = 0
+            hig_weight, low_weight = limit, 0
+
+            while len(count) != 0:
+                if high_weight in count:
+                    if low_weight in count:
+                        num_boats += min(count[low_weight], count[high_weight])
+                        if count[low_weight] == count[high_weight]:
+                            count.remove(low_weight)
+                            count.remobe(high_weight)
+                        elif count[low_weight] > count[high_weight]:
+                            count.remove(high_weight)
+                            count[low_weight] = count[low_weight] - count[high_weight]
+                        else:
+                            count.remove(low_weight)
+                            count[high_weight] = count[high_weight] - count[low_weight]
+                    else:
+                        low_weight -= 1
+                else:
+                    high_weight -=1
+
+            return num_boats
+
+        """
+
+        count = Counter(people)
+        num_boats = 0
+        high_weight, low_weight = max(count.keys()), min(count.keys())
+
+        while len(count) != 0:
+            print(count)
+            # break
+            if high_weight in count:
+                print(high_weight)
+                # break
+                if low_weight in count:
+
+                    if high_weight + low_weight > limit:
+                        num_boats += count[high_weight]
+                        count.pop(high_weight)
+                        high_weight -= 1
+                    elif high_weight == low_weight:
+                        num_boats += count[high_weight] //2 + count[high_weight]%2
+                        count.pop(high_weight)
+                    else:
+                        num_boats += min(count[low_weight], count[high_weight])
+                        # print(num_boats)
+                        if count[low_weight] == count[high_weight]:
+                            count.pop(low_weight)
+                            count.pop(high_weight)
+                        elif count[low_weight] > count[high_weight]:
+                            count[low_weight] = count[low_weight] - count[high_weight]
+                            count.pop(high_weight)
+                        else:
+                            count[high_weight] = count[high_weight] - count[low_weight]
+                            count.pop(low_weight)
+
+                else:
+                    low_weight += 1
+            else:
+
+                high_weight -=1
+
+        return num_boats
+            
+
