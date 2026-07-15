261d6be50c9f3d96788a8bc17413f76c14e1d86e
Maxime Bonnesoeur <44313994+peterbonnesoeur@users.noreply.github.com>
2026-07-15 09:50:11 +0200

    Add: course-schedule-iv - submission-5

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-5.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        adj = defaultdict(list)
+
+        for courses in prerequisites:
+            adj[courses[0]].append(courses[1])
+
+        # visiting = set()
+
+        # OR we precomputre it...
+
+        def dfs(course) -> set[int]:
+            if course not in preMap:
+                for prereq in adj[course]:
+                    preMap[course] |= dfs(prereq)
+                # ultra important -> need to add itself, otherwise,
+                # the children will not know where it came from
+                # This is more clean that passing the prereq in preMap[course]
+                preMap[course].add(course)
+            return preMap[course]
+        preMap = defaultdict(set)
+        for course in range(numCourses):
+            dfs(course)
+        
+        res = []
+        for u,v in queries:
+            res.append(v in preMap[u])
+        return res
+                
+        # def isPrereq(course, target, visited):
+
+        #     if course in visited:
+        #         return False
+        #     visited.add(course)
+
+        #     for pre_course in preMap[course]:
+        #         if pre_course == target:
+        #             return True
+        #         if isPrereq(pre_course, target, visited):
+        #             preMap[course].insert(0, target)
+        #             return True
+        #     return False
+
+        # result = []
+        # for query in queries:
+        #     visited = set()
+        #     if isPrereq(query[0], query[1], visited):
+        #        result.append(True)
+        #     else:
+        #         result.append(False)
+        # return result
