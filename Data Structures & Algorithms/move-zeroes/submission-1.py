5f8ee4fc6403be59881db0f479c9865dbe1478da
Maxime Bonnesoeur <44313994+peterbonnesoeur@users.noreply.github.com>
2026-07-28 07:51:21 +0200

    Add: move-zeroes - submission-1

---
+++ Data Structures & Algorithms/move-zeroes/submission-1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+
+        the goal is to put the zeroes at the end of the array. all the zeroes
+        Without an external array -> we modify things in place.
+
+        What we want to do -> minimise the amount of travel in the array
+        We also want to return the elements in place -> so remembering the past 0s location 
+        Might not help as we can have: [1, 0,0,1,2,0,5] tha mkaes it annoying
+        """
+        
+
+        l = 0
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l] = nums[r]
+                l+=1
+        
+        while l < len(nums):
+            nums[l] = 0
+            l+=1 # Don't forget the state update...
+        
+
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         for j in range(i, len(nums)):
+        #             if nums[j] != 0:
+        #                 nums[i], nums[j] = nums[j], nums[i]
+        #                 break
